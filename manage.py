21b3134069d7a73e938c97cb7be845074f8a3a07
Claudine Akimanizanye <akimanizanye.claudine@gmail.com>
2019-02-27 11:27:26 +0200

    i created  comment class

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 from flask_script import Manager,Server
 from app import create_app,db
 # .....
-from app.models import User,Comment,Pitch,Vote
+from app.models import User,Pitch,Comment,Vote
 # ...
 from  flask_migrate import Migrate, MigrateCommand 
 
@@ -14,19 +14,22 @@ app = create_app('development')
 
 manager = Manager(app)
 manager.add_command('server',Server)
+
+manager.add_command('server',Server)
+
 migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
 
 
-@manager.command
-def test():
-    """Run the unit tests."""
-    import unittest
-    tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity=2).run(tests)
+# @manager.command
+# def test():
+#     """Run the unit tests."""
+#     import unittest
+#     tests = unittest.TestLoader().discover('tests')
+#     unittest.TextTestRunner(verbosity=2).run(tests)
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User )
+    return dict(app = app,db = db,User = User Pitch= Pitch,Comment=Comment,Vote=Vote)
    
 
 if __name__ == '__main__':
