1a16d5aad63de33f1fc8f0e2c2f2e347d3b19c49
Claudine Akimanizanye <akimanizanye.claudine@gmail.com>
2019-02-28 10:41:21 +0200

    i created test

--- app/models.py
+++ app/models.py
@@ -18,7 +18,7 @@ class User(UserMixin,db.Model):
     password_hash = db.Column(db.String(255))
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
-    password_secure = db.Column(db.String(255))
+    pass_secure = db.Column(db.String(255))
     pitches = db.relationship('Pitch',backref = 'user',lazy="dynamic")
     comments = db.relationship('Comment',backref = 'user',lazy="dynamic")
     categories=db.relationship('PitchCategory',backref = 'user',lazy="dynamic")
--- tests/user_test.py
+++ tests/user_test.py
@@ -1,7 +1,7 @@
 import unittest
-from app.models import Movie
+from app.models import User
 
-class MovieTest(unittest.TestCase):
+class UserTest(unittest.TestCase):
     '''
     Test Class to test the behaviour of the Movie class
     '''
@@ -10,10 +10,10 @@ class MovieTest(unittest.TestCase):
         '''
         Set up method that will run before every Test
         '''
-        self.new_movie = Movie(1234,'Python Must Be Crazy','A thrilling new Python Series','/khsjha27hbs',8.5,129993)
+        self.new_user = User(1234,'Python Must Be Crazy','A thrilling new Python Series','/khsjha27hbs',8.5,129993)
 
     def test_instance(self):
-        self.assertTrue(isinstance(self.new_movie,Movie))
+        self.assertTrue(isinstance(self.new_user,User))
 
 if __name__ == '__main__':
     unittest.main()
